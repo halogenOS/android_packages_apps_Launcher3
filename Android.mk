b5f863a93efa0646f195cc0723d7cbb3c6205f49
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Launcher3: lets go Kronic!
    Change-Id: Iaba9d5b30ca8629c06c882549d731eecfce934f4

--- Android.mk
+++ Android.mk
@@ -65,11 +65,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 
 LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src_base) \
     $(call all-java-files-under, src) \
     $(call all-java-files-under, src_shortcuts_overrides) \
     $(call all-java-files-under, src_ui_overrides) \
     $(call all-java-files-under, ext_tests/src)
-    
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/ext_tests/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
