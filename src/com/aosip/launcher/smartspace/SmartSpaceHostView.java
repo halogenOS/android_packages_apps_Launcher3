yaap/packages_apps_Launcher3

CustomLauncher: Replace top search bar with At A Glance
CustomLauncher: Rebind At A Glance when clicking default date widget

[poad42] Adapt for eleven
	* import new logging type introduced in 11 via https://github.com/yaap/packages_apps_Launcher3/commit/b117a9221c1abf81e2e43441a8d8d53f9ccc6d0d and use eventeunum instead of int
	* expose a function in CheckLongPressHelper that smartspace uses

src/com/yaap/launcher/smartspace/SmartSpaceHostView.java
@@ -1,0 +1,155 @@
+package com.yaap.launcher.smartspace;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.view.HapticFeedbackConstants;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import com.android.launcher3.CheckLongPressHelper;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.qsb.QsbWidgetHostView;
+import com.android.launcher3.views.BaseDragLayer;
+import com.android.launcher3.views.OptionsPopupView;
+import com.android.launcher3.logging.StatsLogManager.EventEnum;
+import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS;
+import java.util.Collections;
+
+import static android.view.MotionEvent.*;
+
+public class SmartSpaceHostView extends QsbWidgetHostView implements View.OnLongClickListener, BaseDragLayer.TouchCompleteListener {
+    private static final String SETTINGS_INTENT_ACTION = "com.google.android.apps.gsa.smartspace.SETTINGS";
+    private static final EventEnum SETTINGS_SMARTSPACE = LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS;
+
+    private final Launcher mLauncher;
+    private final CheckLongPressHelper mLongPressHelper = new CheckLongPressHelper(this, this);
+    private final float mSlop = ((float) ViewConfiguration.get(getContext()).getScaledTouchSlop());
+
+    public SmartSpaceHostView(Context context) {
+        super(context);
+        mLauncher = Launcher.getLauncher(context);
+    }
+
+    @Override
+    protected View getErrorView() {
+        return SmartspaceQsbWidget.getDateView(this);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (!hasSettings(v.getContext())) {
+            return false;
+        }
+
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+        Rect rect = new Rect();
+        mLauncher.getDragLayer().getDescendantRectRelativeToSelf(this, rect);
+        RectF rectF = new RectF();
+        float midX = rect.exactCenterX();
+        rectF.right = midX;
+        rectF.left = midX;
+        rectF.top = 0f;
+        rectF.bottom = (float) rect.bottom;
+        rectF.bottom = Math.min((float) findBottomRecur(this, rect.top, rect), rectF.bottom);
+
+        OptionsPopupView.OptionItem item = new OptionsPopupView.OptionItem(
+                R.string.smartspace_preferences,
+                R.drawable.ic_smartspace_preferences,
+                SETTINGS_SMARTSPACE,
+                this::openSettings
+        );
+
+        OptionsPopupView.show(mLauncher, rectF, Collections.singletonList(item));
+        return true;
+    }
+
+    private int findBottomRecur(View view, int i, Rect rect) {
+        if (view.getVisibility() != View.VISIBLE) {
+            return i;
+        }
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int childCount = viewGroup.getChildCount() - 1; childCount >= 0; childCount--) {
+                i = Math.max(findBottomRecur(viewGroup.getChildAt(childCount), i, rect), i);
+            }
+        }
+        if (view.willNotDraw()) {
+            return i;
+        }
+        mLauncher.getDragLayer().getDescendantRectRelativeToSelf(view, rect);
+        return Math.max(i, rect.bottom);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == 0) {
+            mLongPressHelper.cancelLongPress();
+        }
+        if (!mLongPressHelper.hasPerformedLongPress()) {
+            return onTouchEvent(ev);
+        }
+        mLongPressHelper.cancelLongPress();
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        if (action == ACTION_DOWN) {
+            mLongPressHelper.cancelLongPress();
+            mLongPressHelper.postCheckForLongPress();
+            mLauncher.getDragLayer().setTouchCompleteListener(this);
+            return false;
+        }
+        if (action == ACTION_UP) {
+            mLongPressHelper.cancelLongPress();
+            return false;
+        }
+
+        if (action != ACTION_MOVE) {
+            if (action != ACTION_CANCEL) {
+                return false;
+            }
+        } else if (Utilities.pointInView(this, ev.getX(), ev.getY(), mSlop)) {
+            return false;
+        }
+
+        mLongPressHelper.cancelLongPress();
+        return false;
+    }
+
+    @Override
+    public void cancelLongPress() {
+        super.cancelLongPress();
+        mLongPressHelper.cancelLongPress();
+    }
+
+    @Override
+    public void onTouchComplete() {
+        if (!mLongPressHelper.hasPerformedLongPress()) {
+            mLongPressHelper.cancelLongPress();
+        }
+    }
+
+    private boolean openSettings(View v) {
+        v.getContext().startActivity(createSettingsIntent());
+        return true;
+    }
+
+    public static boolean hasSettings(Context context) {
+        return context.getPackageManager().resolveActivity(createSettingsIntent(), 0) != null;
+    }
+
+    public static Intent createSettingsIntent() {
+        return new Intent(SETTINGS_INTENT_ACTION)
+                .setPackage(SmartspaceQsbWidget.WIDGET_PACKAGE_NAME)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_NO_HISTORY
+                        | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+    }
+}

src/com/yaap/launcher/smartspace/SmartspaceQsbWidget.java
@@ -1,0 +1,96 @@
+package com.yaap.launcher.smartspace;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Process;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.android.launcher3.R;
+import com.android.launcher3.qsb.QsbContainerView;
+
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_BIND;
+import static android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID;
+import static android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_PROVIDER;
+
+public class SmartspaceQsbWidget extends QsbContainerView {
+    private static final String WIDGET_CLASS_NAME = "com.google.android.apps.gsa.staticplugins.smartspace.widget.SmartspaceWidgetProvider";
+    public static final String WIDGET_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+
+    public SmartspaceQsbWidget(Context context) {
+        this(context,  null);
+    }
+
+    public SmartspaceQsbWidget(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
+    }
+
+    public SmartspaceQsbWidget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public static class SmartSpaceFragment extends QsbContainerView.QsbFragment {
+        private static final int SMART_SPACE_WIDGET_HOST_ID = 1027;
+        private static final int REQUEST_BIND_QSB = 1;
+
+        private QsbWidgetHost mQsbWidgetHost;
+
+        public SmartSpaceFragment() {
+            mKeyWidgetId = "smart_space_widget_id";
+        }
+
+        @Override
+        protected QsbContainerView.QsbWidgetHost createHost() {
+            mQsbWidgetHost = new QsbContainerView.QsbWidgetHost(getContext(),
+                    SMART_SPACE_WIDGET_HOST_ID,
+                    SmartSpaceHostView::new);
+            return mQsbWidgetHost;
+        }
+
+        @Override
+        protected AppWidgetProviderInfo getSearchWidgetProvider() {
+            for (AppWidgetProviderInfo next : AppWidgetManager.getInstance(getContext()).getInstalledProviders()) {
+                // .getInstalledProvidersForPackage(SmartspaceQsbWidget.WIDGET_PACKAGE_NAME, Process.myUserHandle())
+                if (next.getProfile().equals(Process.myUserHandle())
+                        && SmartspaceQsbWidget.WIDGET_PACKAGE_NAME.equals(next.provider.getPackageName())
+                        && SmartspaceQsbWidget.WIDGET_CLASS_NAME.equals(next.provider.getClassName())) {
+                    return next;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected View getDefaultView(ViewGroup container, boolean showSetupIcon) {
+            View v = SmartspaceQsbWidget.getDateView(container);
+
+            // Return a default widget with setup icon.
+            if (showSetupIcon) {
+                v.setOnClickListener((v2) -> startActivityForResult(
+                        new Intent(ACTION_APPWIDGET_BIND)
+                                .putExtra(EXTRA_APPWIDGET_ID, mQsbWidgetHost.allocateAppWidgetId())
+                                .putExtra(EXTRA_APPWIDGET_PROVIDER, getSearchWidgetProvider().provider),
+                        REQUEST_BIND_QSB));
+            }
+
+            return v;
+        }
+
+        @Override
+        protected Bundle createBindOptions() {
+            Bundle createBindOptions = super.createBindOptions();
+            createBindOptions.putString("attached-launcher-identifier", getContext().getPackageName());
+            createBindOptions.putBoolean("com.google.android.apps.gsa.widget.PREINSTALLED", true);
+            return createBindOptions;
+        }
+    }
+
+    public static View getDateView(ViewGroup root) {
+        return LayoutInflater.from(root.getContext())
+                .inflate(R.layout.smart_space_date_view, root, false);
+    }
+}

halogenOS/android_packages_apps_Launcher3

Change-Id: Ied1cf7055b3fdda3b29ac84e7cf8ce801df1a86b

src/com/aosip/launcher/smartspace/SmartSpaceHostView.java
@@ -1,0 +1,155 @@
+package com.aosip.launcher.smartspace;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.view.HapticFeedbackConstants;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import com.android.launcher3.CheckLongPressHelper;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.qsb.QsbWidgetHostView;
+import com.android.launcher3.views.BaseDragLayer;
+import com.android.launcher3.views.OptionsPopupView;
+import com.android.launcher3.logging.StatsLogManager.EventEnum;
+import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS;
+import java.util.Collections;
+
+import static android.view.MotionEvent.*;
+
+public class SmartSpaceHostView extends QsbWidgetHostView implements View.OnLongClickListener, BaseDragLayer.TouchCompleteListener {
+    private static final String SETTINGS_INTENT_ACTION = "com.google.android.apps.gsa.smartspace.SETTINGS";
+    private static final EventEnum SETTINGS_SMARTSPACE = LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS;
+
+    private final Launcher mLauncher;
+    private final CheckLongPressHelper mLongPressHelper = new CheckLongPressHelper(this, this);
+    private final float mSlop = ((float) ViewConfiguration.get(getContext()).getScaledTouchSlop());
+
+    public SmartSpaceHostView(Context context) {
+        super(context);
+        mLauncher = Launcher.getLauncher(context);
+    }
+
+    @Override
+    protected View getErrorView() {
+        return SmartspaceQsbWidget.getDateView(this);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (!hasSettings(v.getContext())) {
+            return false;
+        }
+
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+        Rect rect = new Rect();
+        mLauncher.getDragLayer().getDescendantRectRelativeToSelf(this, rect);
+        RectF rectF = new RectF();
+        float midX = rect.exactCenterX();
+        rectF.right = midX;
+        rectF.left = midX;
+        rectF.top = 0f;
+        rectF.bottom = (float) rect.bottom;
+        rectF.bottom = Math.min((float) findBottomRecur(this, rect.top, rect), rectF.bottom);
+
+        OptionsPopupView.OptionItem item = new OptionsPopupView.OptionItem(
+                R.string.smartspace_preferences,
+                R.drawable.ic_smartspace_preferences,
+                SETTINGS_SMARTSPACE,
+                this::openSettings
+        );
+
+        OptionsPopupView.show(mLauncher, rectF, Collections.singletonList(item));
+        return true;
+    }
+
+    private int findBottomRecur(View view, int i, Rect rect) {
+        if (view.getVisibility() != View.VISIBLE) {
+            return i;
+        }
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int childCount = viewGroup.getChildCount() - 1; childCount >= 0; childCount--) {
+                i = Math.max(findBottomRecur(viewGroup.getChildAt(childCount), i, rect), i);
+            }
+        }
+        if (view.willNotDraw()) {
+            return i;
+        }
+        mLauncher.getDragLayer().getDescendantRectRelativeToSelf(view, rect);
+        return Math.max(i, rect.bottom);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == 0) {
+            mLongPressHelper.cancelLongPress();
+        }
+        if (!mLongPressHelper.hasPerformedLongPress()) {
+            return onTouchEvent(ev);
+        }
+        mLongPressHelper.cancelLongPress();
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        if (action == ACTION_DOWN) {
+            mLongPressHelper.cancelLongPress();
+            mLongPressHelper.postCheckForLongPress();
+            mLauncher.getDragLayer().setTouchCompleteListener(this);
+            return false;
+        }
+        if (action == ACTION_UP) {
+            mLongPressHelper.cancelLongPress();
+            return false;
+        }
+
+        if (action != ACTION_MOVE) {
+            if (action != ACTION_CANCEL) {
+                return false;
+            }
+        } else if (Utilities.pointInView(this, ev.getX(), ev.getY(), mSlop)) {
+            return false;
+        }
+
+        mLongPressHelper.cancelLongPress();
+        return false;
+    }
+
+    @Override
+    public void cancelLongPress() {
+        super.cancelLongPress();
+        mLongPressHelper.cancelLongPress();
+    }
+
+    @Override
+    public void onTouchComplete() {
+        if (!mLongPressHelper.hasPerformedLongPress()) {
+            mLongPressHelper.cancelLongPress();
+        }
+    }
+
+    private boolean openSettings(View v) {
+        v.getContext().startActivity(createSettingsIntent());
+        return true;
+    }
+
+    public static boolean hasSettings(Context context) {
+        return context.getPackageManager().resolveActivity(createSettingsIntent(), 0) != null;
+    }
+
+    public static Intent createSettingsIntent() {
+        return new Intent(SETTINGS_INTENT_ACTION)
+                .setPackage(SmartspaceQsbWidget.WIDGET_PACKAGE_NAME)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_NO_HISTORY
+                        | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+    }
+}

src/com/aosip/launcher/smartspace/SmartspaceQsbWidget.java
@@ -1,0 +1,96 @@
+package com.aosip.launcher.smartspace;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Process;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.android.launcher3.R;
+import com.android.launcher3.qsb.QsbContainerView;
+
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_BIND;
+import static android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID;
+import static android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_PROVIDER;
+
+public class SmartspaceQsbWidget extends QsbContainerView {
+    private static final String WIDGET_CLASS_NAME = "com.google.android.apps.gsa.staticplugins.smartspace.widget.SmartspaceWidgetProvider";
+    public static final String WIDGET_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+
+    public SmartspaceQsbWidget(Context context) {
+        this(context,  null);
+    }
+
+    public SmartspaceQsbWidget(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
+    }
+
+    public SmartspaceQsbWidget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public static class SmartSpaceFragment extends QsbContainerView.QsbFragment {
+        private static final int SMART_SPACE_WIDGET_HOST_ID = 1027;
+        private static final int REQUEST_BIND_QSB = 1;
+
+        private QsbWidgetHost mQsbWidgetHost;
+
+        public SmartSpaceFragment() {
+            mKeyWidgetId = "smart_space_widget_id";
+        }
+
+        @Override
+        protected QsbContainerView.QsbWidgetHost createHost() {
+            mQsbWidgetHost = new QsbContainerView.QsbWidgetHost(getContext(),
+                    SMART_SPACE_WIDGET_HOST_ID,
+                    SmartSpaceHostView::new);
+            return mQsbWidgetHost;
+        }
+
+        @Override
+        protected AppWidgetProviderInfo getSearchWidgetProvider() {
+            for (AppWidgetProviderInfo next : AppWidgetManager.getInstance(getContext()).getInstalledProviders()) {
+                // .getInstalledProvidersForPackage(SmartspaceQsbWidget.WIDGET_PACKAGE_NAME, Process.myUserHandle())
+                if (next.getProfile().equals(Process.myUserHandle())
+                        && SmartspaceQsbWidget.WIDGET_PACKAGE_NAME.equals(next.provider.getPackageName())
+                        && SmartspaceQsbWidget.WIDGET_CLASS_NAME.equals(next.provider.getClassName())) {
+                    return next;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected View getDefaultView(ViewGroup container, boolean showSetupIcon) {
+            View v = SmartspaceQsbWidget.getDateView(container);
+
+            // Return a default widget with setup icon.
+            if (showSetupIcon) {
+                v.setOnClickListener((v2) -> startActivityForResult(
+                        new Intent(ACTION_APPWIDGET_BIND)
+                                .putExtra(EXTRA_APPWIDGET_ID, mQsbWidgetHost.allocateAppWidgetId())
+                                .putExtra(EXTRA_APPWIDGET_PROVIDER, getSearchWidgetProvider().provider),
+                        REQUEST_BIND_QSB));
+            }
+
+            return v;
+        }
+
+        @Override
+        protected Bundle createBindOptions() {
+            Bundle createBindOptions = super.createBindOptions();
+            createBindOptions.putString("attached-launcher-identifier", getContext().getPackageName());
+            createBindOptions.putBoolean("com.google.android.apps.gsa.widget.PREINSTALLED", true);
+            return createBindOptions;
+        }
+    }
+
+    public static View getDateView(ViewGroup root) {
+        return LayoutInflater.from(root.getContext())
+                .inflate(R.layout.smart_space_date_view, root, false);
+    }
+}