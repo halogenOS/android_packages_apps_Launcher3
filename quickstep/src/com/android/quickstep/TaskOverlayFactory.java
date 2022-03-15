23e435c44b5acb851fdf9ec841e33dc766844a17
jhonboy121 <alfredmathew05@gmail.com>
2022-03-15 18:55:35 +0530

    Launcher3: Show clear all button in recents overview
    idoybh (YAAP): Keep rightmost
    neobuddy89: Updated for A15 QPR2
    xdevs23: Updated for 16
    
    Change-Id: I3291f91a9b4656a28c3f7e4c497026f4248b6ae7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Noah Anleitner <noah.anleitner@nonetwor.cc>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -257,6 +257,11 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
                     .saveAppPair(taskView);
         }
 
+        private void clearAllTasks() {
+            final RecentsView recentsView = mTaskContainer.getTaskView().getRecentsView();
+            recentsView.dismissAllTasks();
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -403,16 +408,24 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             }
 
             @SuppressLint("NewApi")
+            @Override
             public void onScreenshot() {
                 endLiveTileMode(() -> saveScreenshot(mTask));
             }
 
+            @Override
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
             }
 
+            @Override
             public void onSaveAppPair() {
                 endLiveTileMode(TaskOverlay.this::saveAppPair);
+            }
+
+            @Override
+            public void onClearAllTasksRequested() {
+                endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
         }
     }
@@ -430,5 +443,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
         /** User wants to save an app pair with current group of apps. */
         void onSaveAppPair();
+
+        void onClearAllTasksRequested();
     }
 }
