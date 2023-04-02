278eb5fe7bab335bd5bb73eeaaddf7b44d1f5387
LordShenron <shen.priyanshu@gmail.com>
2023-04-02 20:16:50 +0800

    DerpLauncher: Vibrate on double tap to sleep
    [HELLBOY017: Make it optional]
    
    Change-Id: I613edd50bd51c59fee4a11ee2534ff054ebb6125
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -51,6 +51,7 @@ import com.android.launcher3.logger.LauncherAtom;
 import com.android.launcher3.testing.TestLogging;
 import com.android.launcher3.testing.shared.TestProtocol;
 import com.android.launcher3.util.TouchUtil;
+import com.android.launcher3.util.VibratorWrapper;
 
 /**
  * Helper class to handle touch on empty space in workspace and show options popup on long press
@@ -70,6 +71,7 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     private static final int STATE_COMPLETED = 3;
 
     private static final String SLEEP_GESTURE = "pref_sleep_gesture";
+    private static final String HAPTICS_ON_DT2S = "pref_haptics_on_dt2s";
 
     private final Rect mTempRect = new Rect();
     private final Launcher mLauncher;
@@ -236,8 +238,12 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        if (LauncherPrefs.getPrefs(mContext).getBoolean(SLEEP_GESTURE, true))
+        if (LauncherPrefs.getPrefs(mContext).getBoolean(SLEEP_GESTURE, true)) {
+            if (LauncherPrefs.getPrefs(mContext).getBoolean(HAPTICS_ON_DT2S, true)) {
+                VibratorWrapper.INSTANCE.get(mContext).vibrate(VibratorWrapper.EFFECT_CLICK);
+            }
             mPm.goToSleep(event.getEventTime());
+        }
         return true;
     }
 }
