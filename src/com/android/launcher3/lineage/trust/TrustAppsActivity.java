DerpFest-AOSP/packages_apps_DerpLauncher

DerpLauncher: Implement hidden & protected apps
* DerpLauncher: implement hidden apps
* DerpLauncher: Implement protected apps

Squashed with:

Author: Luca Stefani <luca.stefani.ge1@gmail.com>
Date:   Sat Nov 2 15:23:42 2019 +0100

    Switch to BiometricPrompt

    Change-Id: Id6700c81aadb64434aecf1e54ae8b0f01d8143fb

Author: Joey <joey@lineageos.org>
Date:   Fri Feb 7 22:18:51 2020 +0100

    Trebuchet: fix protected apps behaviour

    If no secure lockscreen is set we should not show
    the app list

    Also make icons' touchable area larger

    Change-Id: Id398e98cb4d8fdae169aae194211b98e6441b4cf
    Signed-off-by: Joey <joey@lineageos.org>

Author: Joey <joey@lineageos.org>
Date:   Sat Apr 4 10:57:37 2020 +0200

    Trebuchet: only allow hidden apps when no secure keyguard is set

    Change-Id: I670a61ff3fee4d7c7d9623ce8810790f1b25224a
    Signed-off-by: Joey <joey@lineageos.org>

Author: Joey <joey@lineageos.org>
Date:   Wed Aug 5 13:23:03 2020 +0200

    Launcher3: improve protected app names style

    Change-Id: I09e9995f0eb34de23353166e302481d064fb3058
    Signed-off-by: Joey <joey@lineageos.org>

Author: programminghoch10 <hoch10@kabelbw.de>
Date:   Mon Apr 18 12:39:22 2022 +0200

    Trebuchet: adapt hidden and protected apps for S

    * Specifying the AppFilter class in the config.xml does not work anymore
    * Instead we just instantiate our own HiddenAppsFilter class in the LauncherModel

    Change-Id: I2a6791083261a580327d99dd1e93eaa019de5bc8

Signed-off-by: Joey <joey@lineageos.org>
Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd

src/com/android/launcher3/lineage/trust/TrustAppsActivity.java
@@ -1,0 +1,157 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.lineage.trust;
+
+import static com.android.launcher3.lineage.trust.db.TrustComponent.Kind.HIDDEN;
+import static com.android.launcher3.lineage.trust.db.TrustComponent.Kind.PROTECTED;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.launcher3.LauncherAppState;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.lineage.LineageUtils;
+import com.android.launcher3.lineage.trust.db.TrustComponent;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
+
+import java.util.List;
+
+public class TrustAppsActivity extends Activity implements
+        TrustAppsAdapter.Listener,
+        LoadTrustComponentsTask.Callback,
+        UpdateItemTask.UpdateCallback {
+
+    private static final String KEY_TRUST_ONBOARDING = "pref_trust_onboarding";
+
+    private RecyclerView mRecyclerView;
+    private LinearLayout mLoadingView;
+    private ProgressBar mProgressBar;
+
+    private TrustDatabaseHelper mDbHelper;
+    private TrustAppsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        setContentView(R.layout.activity_hidden_apps);
+        mRecyclerView = findViewById(R.id.hidden_apps_list);
+        mLoadingView = findViewById(R.id.hidden_apps_loading);
+        mLoadingView.setVisibility(View.VISIBLE);
+        mProgressBar = findViewById(R.id.hidden_apps_progress_bar);
+
+        final boolean hasSecureKeyguard = LineageUtils.hasSecureKeyguard(this);
+        mAdapter = new TrustAppsAdapter(this, hasSecureKeyguard);
+        mDbHelper = TrustDatabaseHelper.getInstance(this);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+
+        showOnBoarding(false);
+
+        new LoadTrustComponentsTask(mDbHelper, getPackageManager(), this).execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_trust_apps, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.menu_trust_help) {
+            showOnBoarding(true);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onHiddenItemChanged(@NonNull TrustComponent component) {
+        new UpdateItemTask(mDbHelper, this, HIDDEN).execute(component);
+    }
+
+    @Override
+    public void onProtectedItemChanged(@NonNull TrustComponent component) {
+        new UpdateItemTask(mDbHelper, this, PROTECTED).execute(component);
+    }
+
+    @Override
+    public void onUpdated(boolean result) {
+        LauncherAppState state = LauncherAppState.getInstanceNoCreate();
+        if (state != null) {
+            state.getModel().forceReload();
+        }
+    }
+
+    @Override
+    public void onLoadListProgress(int progress) {
+        mProgressBar.setProgress(progress);
+    }
+
+    @Override
+    public void onLoadCompleted(List<TrustComponent> result) {
+        mLoadingView.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mAdapter.update(result);
+    }
+
+    private void showOnBoarding(boolean forceShow) {
+        SharedPreferences preferenceManager = Utilities.getPrefs(this);
+        if (!forceShow && preferenceManager.getBoolean(KEY_TRUST_ONBOARDING, false)) {
+            return;
+        }
+
+        preferenceManager.edit()
+                .putBoolean(KEY_TRUST_ONBOARDING, true)
+                .apply();
+
+        new AlertDialog.Builder(this)
+                .setView(R.layout.dialog_trust_welcome)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+}

halogenOS/android_packages_apps_Launcher3

Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

src/com/android/launcher3/lineage/trust/TrustAppsActivity.java
@@ -1,0 +1,157 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.lineage.trust;
+
+import static com.android.launcher3.lineage.trust.db.TrustComponent.Kind.HIDDEN;
+import static com.android.launcher3.lineage.trust.db.TrustComponent.Kind.PROTECTED;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.launcher3.LauncherAppState;
+import com.android.launcher3.LauncherPrefs;
+import com.android.launcher3.R;
+import com.android.launcher3.lineage.LineageUtils;
+import com.android.launcher3.lineage.trust.db.TrustComponent;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
+
+import java.util.List;
+
+public class TrustAppsActivity extends Activity implements
+        TrustAppsAdapter.Listener,
+        LoadTrustComponentsTask.Callback,
+        UpdateItemTask.UpdateCallback {
+
+    private static final String KEY_TRUST_ONBOARDING = "pref_trust_onboarding";
+
+    private RecyclerView mRecyclerView;
+    private LinearLayout mLoadingView;
+    private ProgressBar mProgressBar;
+
+    private TrustDatabaseHelper mDbHelper;
+    private TrustAppsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        setContentView(R.layout.activity_hidden_apps);
+        mRecyclerView = findViewById(R.id.hidden_apps_list);
+        mLoadingView = findViewById(R.id.hidden_apps_loading);
+        mLoadingView.setVisibility(View.VISIBLE);
+        mProgressBar = findViewById(R.id.hidden_apps_progress_bar);
+
+        final boolean hasSecureKeyguard = LineageUtils.hasSecureKeyguard(this);
+        mAdapter = new TrustAppsAdapter(this, hasSecureKeyguard);
+        mDbHelper = TrustDatabaseHelper.getInstance(this);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+
+        showOnBoarding(false);
+
+        new LoadTrustComponentsTask(mDbHelper, getPackageManager(), this).execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_trust_apps, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.menu_trust_help) {
+            showOnBoarding(true);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onHiddenItemChanged(@NonNull TrustComponent component) {
+        new UpdateItemTask(mDbHelper, this, HIDDEN).execute(component);
+    }
+
+    @Override
+    public void onProtectedItemChanged(@NonNull TrustComponent component) {
+        new UpdateItemTask(mDbHelper, this, PROTECTED).execute(component);
+    }
+
+    @Override
+    public void onUpdated(boolean result) {
+        LauncherAppState state = LauncherAppState.getInstanceNoCreate();
+        if (state != null) {
+            state.getModel().forceReload();
+        }
+    }
+
+    @Override
+    public void onLoadListProgress(int progress) {
+        mProgressBar.setProgress(progress);
+    }
+
+    @Override
+    public void onLoadCompleted(List<TrustComponent> result) {
+        mLoadingView.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mAdapter.update(result);
+    }
+
+    private void showOnBoarding(boolean forceShow) {
+        SharedPreferences preferenceManager = LauncherPrefs.getPrefs(this);
+        if (!forceShow && preferenceManager.getBoolean(KEY_TRUST_ONBOARDING, false)) {
+            return;
+        }
+
+        preferenceManager.edit()
+                .putBoolean(KEY_TRUST_ONBOARDING, true)
+                .apply();
+
+        new AlertDialog.Builder(this)
+                .setView(R.layout.dialog_trust_welcome)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+}