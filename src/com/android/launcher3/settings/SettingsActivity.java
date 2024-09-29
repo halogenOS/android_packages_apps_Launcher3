ab7dab36d50428b8cf0e6dc37ce80b47f8f104a0
N0m4n904 <noah.anleitner@halogenos.org>
2024-09-29 17:29:54 +0200

    fixup! DerpLauncher: Implement hidden & protected apps
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>
    Signed-off-by: Noah Anleitner <noah.anleitner@nonetwor.cc>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -56,6 +56,8 @@ import com.android.launcher3.Flags;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
@@ -84,6 +86,8 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
