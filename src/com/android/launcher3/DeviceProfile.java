0819e31407ef5e19fdbd66161bb8efcdcc22ae4e
Simão Gomes Viana <devel@superboring.dev>
2026-03-11 01:01:09 +0100

    Launcher3: Refine all apps search bar and top padding
    Use Launcher3-local materialColor resources instead of framework
    theme attributes that don't exist in the android namespace.
    
    Add horizontal margins to the search bar for better spacing.
    
    Adjust top padding: use margin+pill instead of margin+pill+margin
    for the sheet handle area offset, and increase the floating search
    additional top padding from 16dp to 24dp.

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -407,7 +407,8 @@ public class DeviceProfile {
         mBubbleBarSpaceThresholdPx =
                 res.getDimensionPixelSize(R.dimen.bubblebar_hotseat_adjustment_threshold);
 
-        int allAppsTopPadding = -res.getDimensionPixelSize(R.dimen.bottom_sheet_handle_area_height);
+        int allAppsTopPadding = -(res.getDimensionPixelSize(R.dimen.bottom_sheet_handle_margin)
+                + res.getDimensionPixelSize(R.dimen.bottom_sheet_handle_height));
 
         // Needs to be calculated after hotseatBarSizePx is correct,
         // for the available height to be correct
