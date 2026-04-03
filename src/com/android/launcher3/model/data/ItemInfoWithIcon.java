455a6e83f513c5f92ee344dcd9eb06014b5657e5
Simão Gomes Viana <devel@superboring.dev>
2026-04-03 21:00:38 +0200

    Set app drawer icons to the same shape as home screen ones

--- src/com/android/launcher3/model/data/ItemInfoWithIcon.java
+++ src/com/android/launcher3/model/data/ItemInfoWithIcon.java
@@ -341,7 +341,6 @@ public abstract class ItemInfoWithIcon extends ItemInfo {
      */
     public boolean supportsCustomShapes(@DrawableCreationFlags int creationFlags) {
         return Flags.enableLauncherIconShapes()
-                && (creationFlags & FLAG_THEMED) != 0
                 && bitmap.isFullBleed();
     }
 
